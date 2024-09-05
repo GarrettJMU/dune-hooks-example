82ba466fee291c5ccc0b5a61962cf3b28d6b71bb
Garrett Hughes <garretthughes3@gmail.com>
2024-09-05 13:34:49 -0400

    ini

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
+import { DuneProvider } from '@duneanalytics/hooks';
 import { config } from '../wagmi';
 
 const client = new QueryClient();
@@ -13,11 +13,13 @@ const client = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
+      <DuneProvider duneApiKey="foo">
       <QueryClientProvider client={client}>
         <RainbowKitProvider>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
+      </DuneProvider>
     </WagmiProvider>
   );
 }
